7369659d2af88ee8e38dc9624c8a5ce4f917b148
ZacsGuy <ewilliams8222@gmail.com>
2026-02-19 15:55:17 +1300

    chore: snapshot 18/40 (2026-02-19)

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,10 @@
+export const TIMESTAMP_TOLERANCE_SECONDS = 300;
+export const DEFAULT_API_BASE_URL = "https://api.clawdrive.dev";
+
+export const HEADER_KEY_HASH = "x-clawdrive-keyhash";
+export const HEADER_TIMESTAMP = "x-clawdrive-timestamp";
+export const HEADER_NONCE = "x-clawdrive-nonce";
+export const HEADER_SIGNATURE = "x-clawdrive-signature";
+
+export const GRANT_ISSUER = "clawdrive";
+export const GRANT_DEFAULT_TTL_SECONDS = 300;
